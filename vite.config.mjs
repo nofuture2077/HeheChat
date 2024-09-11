e07749ed3fe2a9fb198e466b9ece439047b57ec1
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-09-11 17:41:56 +0200

    pwa

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), mkcert()],
+  plugins: [react(), tsconfigPaths(), mkcert(), VitePWA({ registerType: 'autoUpdate' })],
   base: '/HeheChat/',
   test: {
     globals: true,
