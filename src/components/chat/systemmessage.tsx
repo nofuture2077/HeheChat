52b42825f4eb38853a97709b2e3714b92b0b5583
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-09-13 14:58:55 +0200

    sub message styling

--- src/components/chat/systemmessage.tsx
+++ src/components/chat/systemmessage.tsx
@@ -76,8 +76,11 @@ export function SystemMessageComp(props: SystemMessageProps) {
 
     const actions = (props.msg.subType === 'raid' && canShoutout && modToolsEnabled) ? <ActionIcon key='shoutoutAction' variant='subtle' color='primary' size={26} m="0 6px" onClick={() => props.modActions.shoutoutUser(props.msg.channelId, props.msg.userId)}><IconSpeakerphone size={22} /></ActionIcon> : null;
     return <div className={[classes.msg, classes[props.msg.subType]].join(' ')}>
-            <Text fw={700} {...style}>{emotes.getLogo(parts[1])}{textParts[0]}</Text>
-            {textParts.length === 2 ? <Text fw={500}>{parsedPartsToHtml(msgParts, channel, emotes, login)}</Text>: null}
-            {actions}
+            <div className={classes.action}>
+                <Text fw={700} {...style}>{emotes.getLogo(parts[1])}{textParts[0]}</Text>
+                {actions}
+            </div>
+            {textParts.length === 2 ? <Text inline={false} fw={500}>{parsedPartsToHtml(msgParts, channel, emotes, login)}</Text>: null}
+            
         </div>;
 }
