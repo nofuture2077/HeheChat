252907b756e280b0d39415e70ed1c3433ed77b54
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-09-19 10:47:36 +0200

    meta tags

--- vite.config.mjs
+++ vite.config.mjs
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#480356',
         icons: [
           {
-            src: 'android-chrome-192x192.png',
+            src: 'images/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'images/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
