2a4dbe2c8cf94c1294707b72d2a1ad2be586c8ac
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-10-13 19:14:59 +0200

    manifest

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import mkcert from 'vite-plugin-mkcert';
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   build: {
@@ -13,27 +12,7 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react(), tsconfigPaths(), mkcert(), VitePWA({ 
-    registerType: 'autoUpdate',
-      manifest: {
-        name: 'Hehe Chat',
-        short_name: 'Hehe',
-        description: 'Twitchs finest chat app',
-        theme_color: '#480356',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-  })],
+  plugins: [react(), tsconfigPaths(), mkcert()],
   base: '#{import.meta.env.VITE_SLUG}',
   test: {
     globals: true,
