deea6d764667b9ff194f7c9cd81fc3d37a430a7c
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-10-13 17:10:27 +0200

    disable router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,13 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import { ChatPage } from './pages/Chat.page';
 import { LoginContextContext } from './ApplicationContext';
 import { useContext } from 'react';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <ChatPage />,
-  }
-], {
-  basename: '#{import.meta.env.VITE_SLUG}'
-});
 
 export function Router() {
   const loginContext = useContext(LoginContextContext);
   if (!loginContext.isLoggedIn()) {
     return <HomePage/>
   }
-  return <RouterProvider router={router} />;
+  return <ChatPage />;
 }
