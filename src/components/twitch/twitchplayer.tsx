7d0c40351401104657b073a33c12258bd2dc4157
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-10-21 15:19:07 +0200

    fix video width

--- src/components/twitch/twitchplayer.tsx
+++ src/components/twitch/twitchplayer.tsx
@@ -1,6 +1,6 @@
 import ReactTwitchEmbedVideo from "react-twitch-embed-video"
 import { useViewportSize } from '@mantine/hooks';
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ConfigContext } from "@/ApplicationContext";
 
 interface TwitchPlayerProps {
@@ -8,9 +8,16 @@ interface TwitchPlayerProps {
 
 export function TwitchPlayer(props: TwitchPlayerProps) {
     const config = useContext(ConfigContext);
-    const { width } = useViewportSize();
-    const w = Math.min(width || 600, 600);
+    const [w, setW] = useState<number>(0);
+   
     const h = (w / 16 * 9);
     const channel = config.getChatChannel();
-    return channel ? <ReactTwitchEmbedVideo channel={channel} layout="video" muted width={w} height={h}/> : null;
+    const { width } = useViewportSize();
+
+    useEffect(() => {
+        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+        const w = Math.min(vw, 600);
+        setW(w)
+    }, []);
+    return channel && w ? <ReactTwitchEmbedVideo channel={channel} layout="video" muted width={w} height={h}/> : null;
 }
