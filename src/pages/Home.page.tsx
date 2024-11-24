f4a2c12e24ae4db144b86ee0fcc74c5891f3a0c9
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-11-24 09:49:40 +0100

    new start page

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,27 +1,29 @@
 import Login from '@/components/login/login';
-import { Stack, Group, Text, Space, Image } from '@mantine/core';
+import { Stack, Group, Text, Image, Affix } from '@mantine/core';
 import classes from './home.module.css'
 import logo from "./logo.svg"
 
 export function HomePage() {
   return (
+    <>
     <Stack className={classes.layout}>
       <Group className={classes.hero} justify='center' pb={60} pt={50}>
-        <Image src={logo} height={192}/>
+        <Image src={logo} height={232}/>
         <Stack>
           <div>
-            <Text ta="center" fw={600} size="92px" style={{marginBottom: 0, letterSpacing: "12px", textShadow: "3px 3px rgba(255, 255, 255, 0.2)"}}>HEHE</Text>
-            <Text ta="center" fw={400} size="68px" style={{marginBottom: "10px", lineHeight: "44px", letterSpacing: "6px", textShadow: "3px 3px rgba(255, 255, 255, 0.2)"}}>CHAT</Text>
-          </div>
-          <div>
-            <Space h="xs"/>
-            <Text fw={300} size="lg">Best multi channel App for Twitch *</Text>
-            <Space h="xs"/>
-            <Text fw={200} size="lg" fs="italic">(*) Alpha #{import.meta.env.VITE_BUILDNUMBER}</Text>
+            <Text ta="center" fw={600} size="92px" style={{color: "rgba(255,255,255, 1)", marginBottom: 0, letterSpacing: "12px", textShadow: "3px 3px rgba(255, 255, 255, 0.3)"}}>HEHE</Text>
+            <Text ta="center" fw={400} size="68px" style={{color: "rgba(255,255,255, 1)", marginBottom: "10px", lineHeight: "44px", letterSpacing: "6px", textShadow: "3px 3px rgba(255, 255, 255, 0.3)"}}>CHAT</Text>
           </div>
         </Stack>
       </Group>
       <Group justify='center' mt={0}><Login /></Group>
     </Stack>
+    <Stack style={{position: 'fixed', bottom: 0, width: '100%', textAlign: 'center'}} gap={0}>
+      <Group style={{textAlign: 'center'}} justify='space-between' p="10">
+        <Text c="dimmed" size="sm" fs="italic">Proudly created by NoFuture</Text>
+        <Text c="dimmed" size="sm" fs="italic">Build: #{import.meta.env.VITE_BUILDNUMBER}</Text>
+      </Group>
+    </Stack>
+    </>
   );
 }
