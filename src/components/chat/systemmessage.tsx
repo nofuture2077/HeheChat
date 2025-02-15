14ed38c6bc01a3671c76600d7979c292dd0a174e
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2025-02-15 09:40:45 +0100

    new touch icon

--- src/components/chat/systemmessage.tsx
+++ src/components/chat/systemmessage.tsx
@@ -84,7 +84,7 @@ export function SystemMessageComp(props: SystemMessageProps) {
     return <div className={[classes.msg, classes[props.msg.subType]].join(' ')}>
                 {config.showProfilePicture ? <span className={classes.logo}>{emotes.getLogo(props.msg.data.channel)}</span> : null}
                 {config.showTimestamp ? <span key='timestamp' className={classes.time}>{formatTime(props.msg.date)} </span> : null}
-                <Text {...style} key="msg-main" fw={700} style={{fontSize: config.fontSize}} span>
+                <Text {...style} key="msg-main" fw={700} style={{fontSize: config.fontSize, color: "light-dark(black, white);"}} span>
                     {joinWithSpace(textParts[0].split(" ").map((value, index, array) => wordMapper(eventType, value, index, array)))}{actions}
                 </Text>
                 {textParts.length === 2 ? <Text key="msg-second" fw={500} style={{fontSize: config.fontSize}}>{parsedPartsToHtml(msgParts, channel, false, config, emotes, login)}</Text>: null}
