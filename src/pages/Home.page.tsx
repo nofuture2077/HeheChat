cd4e1d5b7803db5c84b92e836895dd5b45ca346b
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2025-05-08 12:56:52 +0200

    added version number

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,11 @@
 import Login from '@/components/login/login';
-import { Image, Stack } from '@mantine/core';
+import { Image, Stack, Text } from '@mantine/core';
 import classes from './home.module.css'
 import logosimple from "./LogoSimple.svg"
 import hehechat from "./HEHEChat.svg"
 import nofuture from "./NoFuture.svg"
+// Import version from package.json
+import { version } from '../../package.json';
 
 export function HomePage() {
   return (
@@ -13,7 +15,12 @@ export function HomePage() {
         <Image src={hehechat} className={classes.logo} />
       </Stack>
       <Login color1="#DB32BC" color2="#ff1493"/>
-      <Image src={nofuture} className={classes.nofuture} />
+      <div>
+        <Image src={nofuture} className={classes.nofuture} />
+      </div>
+      <Text size="xs" c="dimmed" style={{ position: 'absolute', bottom: '22px', right: '22px', textAlign: 'center' }}>
+          v{version}
+      </Text>
     </div>
   );
 }
